3b8672bc17beaf7df652c0bd93b7be23a8c79ff9
Brian Amerson <bmamerson@gmail.com>
2025-02-13 07:24:06 -0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,18 +1,16 @@
-﻿using System;
-using System.Threading;
-class Program 
+﻿class Program 
 {
     static string? Ask(string question)
     {
-        Console.Write(question);
+        Console.Write(question + " ");
         return Console.ReadLine();
     }
 
     static void Main(string[] args) 
     {
         Console.WriteLine("Welcome to the cat food store!");
-        
-        string? entry = Ask("How many cans are you ordering? ");
-        Console.WriteLine(entry);
+
+        string? entry = Ask("How many cans are you ordering?");
+        Console.WriteLine($"You entered {entry} cans.");
     }
 }
