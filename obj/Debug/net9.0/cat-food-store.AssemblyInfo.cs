cd96ba447ac1640e04fc356fee454845fb7cfe72
Brian Amerson <bmamerson@gmail.com>
2025-02-13 07:23:39 -0700

    Updated Ask and Main methods

--- obj/Debug/net9.0/cat-food-store.AssemblyInfo.cs
+++ obj/Debug/net9.0/cat-food-store.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cat-food-store")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f07645d057548a0860ac9f48cb8a38a53b6e859d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+114bbc258f4e5b3d91db3db33a5867a272c6ce1b")]
 [assembly: System.Reflection.AssemblyProductAttribute("cat-food-store")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cat-food-store")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
